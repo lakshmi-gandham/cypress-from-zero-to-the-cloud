0d3c96f6080835f31828a69570684f2e9460acc4
Lakshmi <gandham.lakshmi@veeva.com>
2025-02-22 22:17:40 +0530

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,4 +2,5 @@ const { defineConfig } =require('cypress')
 
 module.exports =defineConfig({viewportHeight : 880,
                                viewportWidth: 1280,
+                               projectId:Project ID,
                                e2e: {},})
